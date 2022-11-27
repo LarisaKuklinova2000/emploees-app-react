e0b63651105ea90e5419fbc32f641eb9ece7f8ef
Ivan <gnosticism100500@gmail.com>
2022-11-28 02:13:14 +0500

    add eploees list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,19 +7,27 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
+	const data = [
+		{name: 'Ivan L.', salary: 800, increase: false, id: 1},
+		{name: 'Denis P.', salary: 1200, increase: false, id: 2},
+		{name: 'Alexey Z.', salary: 4000, increase: true, id: 3}
+	]
+
+  	return (
+    	<div className="app">
+
+        	<AppInfo />
+
+        	<div className="search-panel">
+            	<SearchPanel/>
+            	<AppFilter/>
+        	</div>
         
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+        	<EmployeesList data={data} />
+        	<EmployeesAddForm/>
+    	</div>
+  	);
 }
 
 export default App;
